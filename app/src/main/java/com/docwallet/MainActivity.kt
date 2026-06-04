b4baafd2864b69a14000bbe33d331b09ebc482e3
David Neurieder <david.neurieder@zoho.eu>
2026-06-04 09:56:01 +0200

    uses dark light theme of Op system

--- app/src/main/java/com/docwallet/MainActivity.kt
+++ app/src/main/java/com/docwallet/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,7 +34,8 @@ class MainActivity : ComponentActivity() {
         val app = application as DocWalletApplication
 
         setContent {
-            MaterialTheme {
+            val colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+            MaterialTheme(colorScheme = colorScheme) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
